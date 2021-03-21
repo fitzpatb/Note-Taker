c232f85b4a8f89b82938c6851a85b67d81665a6b
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-03-21 13:28:20 -0700

    set up routes for html

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes')(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
